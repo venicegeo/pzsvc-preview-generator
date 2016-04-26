e8c56669c0e6d9cb508de648237639f12118d3ab
Sonny Saniev <sonny.saniev@rgi-corp.com>
2016-04-26 11:45:09 -0400

    adding ErrorResponse model.

---
+++ src/main/java/generator/model/ErrorResponse.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2016, RadiantBlue Technologies, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package generator.model;
+
+/**
+ * Error model for sending back error response.
+ * 
+ * @author Sonny.Saniev
+ * 
+ */
+public class ErrorResponse {
+	private String type = "error";
+	public String message;
+
+	public ErrorResponse(String message) {
+		this.message = message;
+	}
+	
+	/**
+	 * Gets the type of this response.
+	 * 
+	 * @return
+	 */
+	public String getType() {
+		return type;
+	}
+}
