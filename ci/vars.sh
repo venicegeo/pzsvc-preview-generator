cf11f2917420cb353c7976542c91e35857664c63
Patrick D <go0ty@users.noreply.github.com>
2017-07-05 12:58:04 -0400

    Fix vars for promote script

--- ci/vars.sh
+++ ci/vars.sh
@@ -2,3 +2,5 @@
 
 APP=pz-svcs-prevgen
 EXT=jar
+ARTIFACT_STORAGE_URL=https://nexus.devops.geointservices.io/content/repositories/Piazza-Group/
+sed -i "s,\${env.ARTIFACT_STORAGE_URL},$ARTIFACT_STORAGE_URL,g" pom.xml
